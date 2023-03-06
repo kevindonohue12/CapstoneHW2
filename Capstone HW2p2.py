61e2d039be289deba96073ae37fa8be1b218c38b
Kevin Donohue <71093007+kevindonohue12@users.noreply.github.com>
2023-03-06 17:22:39 -0500

    Add files via upload

--- Capstone HW2p2.py
+++ Capstone HW2p2.py
@@ -1,11 +1,11 @@
 import numpy as np
+from numpy import linalg
 import math as m
 #import pandas as pd
-#from numpy import loadtxt
 import matplotlib.pyplot as plt
 #import statsmodels.api as sm
 from scipy.optimize import minimize
-import statsmodels.api as sm
+#import statsmodels.api as sm
 #from mpl_toolkits.mplot3d import Axes3D
 #from mpl_toolkits import mplot3d
 #from scipy.stats import linregress
@@ -22,19 +22,28 @@ for line in open(r"C:\Users\Kevin\OneDrive - Northeastern University\Senior Spri
     x3.append(values[3])
 
 x = np.stack((x1,x2,x3), axis=1)
-x_sum = np.zeros([len(x),1], dtype= float)
+x_sum = np.zeros([len(t),1], dtype= float)
 
 #Fit the data to  𝑏(𝑡) = 𝑏_𝑒𝑞 − [𝑏_𝑒𝑞 − 0.355]𝑒^−𝑘𝑡
 xm = np.mean(x, axis=1)
 xSEM = np.std(x, axis=1)/m.sqrt(25)
 
+tarr = np.array(t)
+#Minimum χ2 (χ2min). Sum of (xm - the function)/ xSEM^2
+def chisquared(vars):
+    return sum(((xm[0:]-(vars[0] - (vars[0]-0.355)*np.power(m.e,-1*vars[1]*tarr[0:])))/(xSEM[0:]))**2)
 
+minchi = minimize(chisquared, np.array([1,1]), method='nelder-mead')
 
-#Find the best values for association rate k and equilibrium extension b_eq. 
+#Reduced χ2, dividing χ2 by the length of x minus number of paramters (b_eq,k)
+reducedchi = minchi.fun / (25-2)
+
  
-#Report the values you found for minimized χ2, reduced χ2, and fitting parameters k and beq, 
-#along with their uncertainties. (Find the uncertainties in the parameters using the χ2min +1 
-#method in two dimensions). Plot the data with error bars. Show the fitted curve. Comment on the 
-#goodness of the fit. 
+#Find the uncertainties in the parameters using the χ2min +1 method in two dimensions
+
+
+
+
+#Plot the data with error bars. Show the fitted curve. Comment on the goodness of the fit. 
 
 
